db0fd2dd8bf0bf6bacab1f4c97b689d0e233bb4b
Andrew Bastien <volundmush@gmail.com>
2020-04-16 20:20:22 -0700

    Beginning work on Athanor.

--- evennia/identities/identities.py
+++ evennia/identities/identities.py
@@ -3,6 +3,7 @@ from evennia.typeclasses.models import TypeclassBase
 from evennia.commands.cmdsethandler import CmdSetHandler
 from evennia.utils.utils import lazy_property
 
+
 class DefaultIdentity(IdentityDB, metaclass=TypeclassBase):
     pass
 
